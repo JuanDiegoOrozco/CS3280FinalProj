259ebbb8c12d30aeecac61f6038e5363e926d09e
Juan Orozco <orozcoj007@gmail.com>
2016-08-05 20:01:00 -0600

    All Files added
    Most of the files are put together. There more things that need to be
    done on this project to start...

---
+++ FinalProject/FinalProject/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace FinalAssignment.ViewModels
+{
+    class MainViewModel : Conductor<IScreen>.Collection.OneActive
+    {
+        public MainViewModel() {
+            this.DisplayName = "Inventory Application";
+        }
+        protected override void OnActivate() {
+            base.OnActivate();
+            Orders();
+        }
+        public void Orders() {
+            var ordersVM = IoC.Get<OrdersViewModel>();
+            //IoC.Get<OrdersViewModel>();
+            OnActivate(ordersVM);
+        }
+    }
+}
